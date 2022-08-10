0da36fdd39640e07a62efffdca085a429e880e80
Ryan Macdonald <ram_idts@me.com>
2022-08-10 10:53:16 +1000

    #5 Added Sidebar component and included it on the index page

---
+++ apps/issues/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export function Sidebar() {
+  return (
+    <aside className="bg-slate-800 flex flex-col h-screen py-4 sticky text-slate-200 top-0 w-48">
+      <span className="font-mono mx-8 mb-4 text-3xl text-center">Issues</span>
+      <div className="border-t border-slate-400 flex-grow mx-4" />
+    </aside>
+  );
+}
--- apps/issues/pages/index.tsx
+++ apps/issues/pages/index.tsx
@@ -1,17 +1,22 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
 
+import { Sidebar } from '../components/Sidebar';
+
 export function Index() {
   const { data } = useSession();
 
   return (
-    <>
-      <h1>Issues</h1>
-      {data ? (
-        <button onClick={() => signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={() => signIn()}>Sign In</button>
-      )}
-    </>
+    <div className="flex">
+      <Sidebar />
+      <main>
+        <h1>Issues</h1>
+        {data ? (
+          <button onClick={() => signOut()}>Sign Out</button>
+        ) : (
+          <button onClick={() => signIn()}>Sign In</button>
+        )}
+      </main>
+    </div>
   );
 }
 
