73c34b0d6cfac2a62f37f74027af09e3340f973b
Ryan Macdonald <ram_idts@me.com>
2022-08-12 10:11:53 +1000

    wip: #10 added simple project list

--- apps/issues/pages/index.tsx
+++ apps/issues/pages/index.tsx
@@ -1,4 +1,10 @@
-import { PlusIcon } from '@heroicons/react/outline';
+import { InformationCircleIcon, PlusIcon } from '@heroicons/react/outline';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import {
   Bar,
   BarChart,
@@ -11,6 +17,12 @@ import {
 
 import { Sidebar } from '../components/Sidebar';
 
+interface Project {
+  title: string;
+  start_date?: Date;
+  end_date?: Date;
+}
+
 interface Ticket {
   title: string;
   project: string;
@@ -21,6 +33,30 @@ interface Ticket {
 }
 
 export function Index() {
+  const today = new Date();
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+
+  const projects: Project[] = [
+    {
+      title: 'issues',
+      start_date: today,
+      end_date: new Date(
+        today.getTime() + Math.ceil(Math.random() * 14) * 24 * 60 * 60 * 1000
+      ),
+    },
+    {
+      title: 'kanban',
+      start_date: today,
+      end_date: new Date(
+        today.getTime() + Math.ceil(Math.random() * 14) * 24 * 60 * 60 * 1000
+      ),
+    },
+  ];
+
   const tickets: Ticket[] = [
     {
       title: 'create dashboard',
@@ -59,13 +95,9 @@ export function Index() {
     },
   ];
 
-  const projects = tickets
-    .map((ticket) => ticket.project)
-    .filter((value, index, self) => self.indexOf(value) === index);
-
   const data = projects.map((project) => {
     const project_tickets = tickets.filter(
-      (ticket) => ticket.project === project
+      (ticket) => ticket.project === project.title
     );
 
     return {
@@ -99,6 +131,39 @@ export function Index() {
         (ticket) => ticket.priority === 'low'
       ).length,
     };
+  });
+
+  const columnHelper = createColumnHelper<Project>();
+  const columns = [
+    columnHelper.accessor('title', {
+      header: () => 'Title',
+      cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor('start_date', {
+      header: () => 'Start Date',
+      cell: (info) => (
+        <div className="text-center">
+          {info.getValue().toLocaleString('en-US', dateOptions)}
+        </div>
+      ),
+    }),
+    columnHelper.accessor('end_date', {
+      header: () => 'End Date',
+      cell: (info) => (
+        <div className="text-center">
+          {info.getValue().toLocaleString('en-US', dateOptions)}
+        </div>
+      ),
+    }),
+    columnHelper.display({
+      id: 'details',
+      cell: () => <InformationCircleIcon className="h-5 mx-auto w-5" />,
+    }),
+  ];
+  const table = useReactTable({
+    columns,
+    data: projects,
+    getCoreRowModel: getCoreRowModel(),
   });
 
   return (
@@ -142,8 +207,8 @@ export function Index() {
             {tickets.filter((ticket) => ticket.assignee === undefined).length}
           </div>
         </div>
-        <div className="bg-slate-50 col-span-4 p-4 rounded-lg">
-          <span>Tickets</span>
+        <div className="bg-slate-50 col-span-4 flex flex-col p-4 rounded-lg space-y-2">
+          <span className="font-medium text-xl text-slate-900">Tickets</span>
           <ResponsiveContainer height={500} width="100%">
             <BarChart
               data={data}
@@ -166,6 +231,40 @@ export function Index() {
               <Bar stackId="priority" dataKey="priority_low" fill="green" />
             </BarChart>
           </ResponsiveContainer>
+        </div>
+        <div className="bg-slate-50 col-span-4 p-4 rounded-lg">
+          <span className="font-medium text-xl text-slate-900">Projects</span>
+          <table className="mt-4 text-slate-800 w-full">
+            <thead className="bg-slate-100 text-slate-900">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id} className="px-4 py-2">
+                      {!header.isPlaceholder &&
+                        flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id} className="border-b">
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id} className="px-2 py-1">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </main>
     </div>
