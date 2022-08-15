c9b50823820bd67c533451f9a5c572548f681d22
Ryan Macdonald <ram_idts@me.com>
2022-08-15 15:42:27 +1000

    wip: #22 generated User validators after adding settings object to schema

--- libs/prisma/src/validators/user.ts
+++ libs/prisma/src/validators/user.ts
@@ -1,12 +1,19 @@
 import * as z from "zod"
 import { CompleteAccount, RelatedAccountModel, CompleteSession, RelatedSessionModel, CompleteUsersInOrganization, RelatedUsersInOrganizationModel } from "./index"
 
+// Helper schema for JSON fields
+type Literal = boolean | number | string
+type Json = Literal | { [key: string]: Json } | Json[]
+const literalSchema = z.union([z.string(), z.number(), z.boolean()])
+const jsonSchema: z.ZodSchema<Json> = z.lazy(() => z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]))
+
 export const UserModel = z.object({
   id: z.string(),
   name: z.string().nullish(),
   email: z.string().nullish(),
   emailVerified: z.date().nullish(),
   image: z.string().nullish(),
+  settings: jsonSchema,
 })
 
 export interface CompleteUser extends z.infer<typeof UserModel> {
