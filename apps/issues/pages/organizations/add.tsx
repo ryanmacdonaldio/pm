c9cf897c796b9fc506f1fdf401e6f7b7c0407a00
iamryanmacdonald <ram_idts@me.com>
2022-10-05 16:03:19 +1100

    wip: #24 removed Layout from add organization page

--- apps/issues/pages/organizations/add.tsx
+++ apps/issues/pages/organizations/add.tsx
@@ -4,7 +4,6 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import FormInput from '../../components/FormInput';
-import Layout from '../../components/Layout';
 import requireLayoutProps from '../../utils/requireLayoutProps';
 import { trpc } from '../../utils/trpc';
 
@@ -27,42 +26,40 @@ function Add() {
   };
 
   return (
-    <Layout>
-      <div className="bg-slate-200 h-screen pt-4">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="bg-slate-50 mx-auto rounded-md shadow-md w-2/3">
-            <div className="col-span-3 border-b font-medium p-4 text-slate-700 text-xl w-full">
-              New Organization
-            </div>
-            <div className="gap-6 grid grid-cols-3 my-4 px-6">
-              <FormInput
-                className="col-span-2 w-full"
-                error={errors.name}
-                label="Name"
-                register={register('name')}
-                type="text"
-              />
-              <FormInput
-                className="col-span-3 w-full"
-                error={errors.description}
-                label="Description"
-                register={register('description')}
-                type="textarea"
-              />
-            </div>
-            <div className="bg-slate-100 px-8 py-4 rounded-b-md text-right">
-              <button
-                type="submit"
-                className="bg-slate-400 font-medium px-4 py-2 rounded-md shadow-sm text-md text-slate-800 hover:bg-slate-500 hover:text-slate-900"
-                disabled={isLoading}
-              >
-                Create
-              </button>
-            </div>
+    <div className="bg-slate-200 h-screen pt-4">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="bg-slate-50 mx-auto rounded-md shadow-md w-2/3">
+          <div className="col-span-3 border-b font-medium p-4 text-slate-700 text-xl w-full">
+            New Organization
           </div>
-        </form>
-      </div>
-    </Layout>
+          <div className="gap-6 grid grid-cols-3 my-4 px-6">
+            <FormInput
+              className="col-span-2 w-full"
+              error={errors.name}
+              label="Name"
+              register={register('name')}
+              type="text"
+            />
+            <FormInput
+              className="col-span-3 w-full"
+              error={errors.description}
+              label="Description"
+              register={register('description')}
+              type="textarea"
+            />
+          </div>
+          <div className="bg-slate-100 px-8 py-4 rounded-b-md text-right">
+            <button
+              type="submit"
+              className="bg-slate-400 font-medium px-4 py-2 rounded-md shadow-sm text-md text-slate-800 hover:bg-slate-500 hover:text-slate-900"
+              disabled={isLoading}
+            >
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
