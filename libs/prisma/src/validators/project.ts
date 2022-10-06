8afa2a939c399bce0a9f84d64a83718a6e80888e
iamryanmacdonald <ram_idts@me.com>
2022-10-06 17:06:12 +1100

    wip: #28 generate zod validators for new model

---
+++ libs/prisma/src/validators/project.ts
@@ -0,0 +1,25 @@
+import * as z from "zod"
+import { CompleteOrganization, RelatedOrganizationModel } from "./index"
+
+export const ProjectModel = z.object({
+  id: z.string(),
+  organizationId: z.string(),
+  name: z.string().nonempty({ message: "Name is required" }),
+  description: z.string().nullish(),
+  startDate: z.date().nullish(),
+  endDate: z.date().nullish(),
+  archived: z.boolean(),
+})
+
+export interface CompleteProject extends z.infer<typeof ProjectModel> {
+  organization: CompleteOrganization
+}
+
+/**
+ * RelatedProjectModel contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const RelatedProjectModel: z.ZodSchema<CompleteProject> = z.lazy(() => ProjectModel.extend({
+  organization: RelatedOrganizationModel,
+}))
