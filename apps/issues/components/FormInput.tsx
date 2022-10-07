f1f97aa10ddfb830dc431299a64f55a556737b45
iamryanmacdonald <ram_idts@me.com>
2022-10-07 13:01:36 +1100

    wip: #28 added project creation page

--- apps/issues/components/FormInput.tsx
+++ apps/issues/components/FormInput.tsx
@@ -15,23 +15,40 @@ function FormInput({
   register,
   type,
 }: FormInputProps) {
+  const renderInput = () => {
+    switch (type) {
+      case 'checkbox':
+        return (
+          <div className="flex mt-1">
+            <input type="checkbox" className="h-5 ml-1 w-5" {...register} />
+          </div>
+        );
+      case 'textarea':
+        return (
+          <div className="flex mt-1 rounded-md shadow-sm">
+            <textarea
+              className="block border border-slate-300 flex-1 outline-none px-3 py-2 rounded-none rounded-r-md w-full focus:border-slate-400"
+              {...register}
+            ></textarea>
+          </div>
+        );
+      default:
+        return (
+          <div className="flex mt-1 rounded-md shadow-sm">
+            <input
+              type={type}
+              className="block border border-slate-300 flex-1 outline-none px-3 py-2 rounded-none rounded-r-md w-full focus:border-slate-400"
+              {...register}
+            />
+          </div>
+        );
+    }
+  };
+
   return (
     <div className={className}>
       <label className="block font-medium text-slate-600">{label}</label>
-      <div className="flex mt-1 rounded-md shadow-sm">
-        {type === 'textarea' ? (
-          <textarea
-            className="block border border-slate-300 flex-1 outline-none px-3 py-2 rounded-none rounded-r-md w-full focus:border-slate-400"
-            {...register}
-          ></textarea>
-        ) : (
-          <input
-            type={type}
-            className="block border border-slate-300 flex-1 outline-none px-3 py-2 rounded-none rounded-r-md w-full focus:border-slate-400"
-            {...register}
-          />
-        )}
-      </div>
+      {renderInput()}
       {error && <pre className="text-red-700">{error.message}</pre>}
     </div>
   );
