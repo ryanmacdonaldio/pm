105d77d2c1ef630b929091253c94cf42f7f78c75
iamryanmacdonald <ram_idts@me.com>
2022-10-11 13:00:40 +1100

    wip: #28 created links for Add Project buttons

--- apps/issues/pages/projects/all.tsx
+++ apps/issues/pages/projects/all.tsx
@@ -1,6 +1,7 @@
 import { PlusIcon } from '@heroicons/react/outline';
-import { ProjectList } from '../../components/ProjectList';
+import Link from 'next/link';
 
+import { ProjectList } from '../../components/ProjectList';
 import requireLayoutProps from '../../utils/requireLayoutProps';
 import { trpc } from '../../utils/trpc';
 
@@ -13,10 +14,12 @@ function All() {
         <span className="font-medium text-2xl text-slate-900">
           All Projects
         </span>
-        <button className="bg-blue-100 border-2 border-blue-400 flex items-center px-3 py-1 rounded-md space-x-2 text-blue-900">
-          <PlusIcon className="h-3 w-3" />
-          <span>Add Project</span>
-        </button>
+        <Link href="/projects/add">
+          <button className="bg-blue-100 border-2 border-blue-400 flex items-center px-3 py-1 rounded-md space-x-2 text-blue-900">
+            <PlusIcon className="h-3 w-3" />
+            <span>Add Project</span>
+          </button>
+        </Link>
       </div>
       <ProjectList isLoading={isLoading} projects={projects} />
     </div>
