a9aab50817c76d5b90fe18c182bb7d78bfdc2357
iamryanmacdonald <ram_idts@me.com>
2022-10-13 12:17:50 +1100

    wip: #30 dashboard now pulls ticket data

--- apps/issues/pages/index.tsx
+++ apps/issues/pages/index.tsx
@@ -1,4 +1,5 @@
 import { PlusIcon } from '@heroicons/react/outline';
+import { Project } from '@prisma/client';
 import Link from 'next/link';
 import {
   Bar,
@@ -14,92 +15,43 @@ import { ProjectList } from '../components/ProjectList';
 import requireLayoutProps from '../utils/requireLayoutProps';
 import { trpc } from '../utils/trpc';
 
-interface Ticket {
-  title: string;
-  project: string;
-  status: string;
-  type: string;
-  priority: string;
-  assignee?: string;
-}
-
 function Index() {
-  const { data: projects, isLoading } = trpc.project.getAll.useQuery();
-
-  const tickets: Ticket[] = [
-    {
-      title: 'create dashboard',
-      project: 'issues',
-      status: 'code',
-      type: 'feat',
-      priority: 'medium',
-    },
-    {
-      title: 'create projects page',
-      project: 'issues',
-      status: 'backlog',
-      type: 'feat',
-      priority: 'medium',
-    },
-    {
-      title: 'create tickets page',
-      project: 'issues',
-      status: 'backlog',
-      type: 'feat',
-      priority: 'medium',
-    },
-    {
-      title: 'fix sidebar dropdown animation',
-      project: 'issues',
-      status: 'backlog',
-      type: 'fix',
-      priority: 'high',
-    },
-    {
-      title: 'create kanban board',
-      project: 'kanban',
-      status: 'backlog',
-      type: 'feat',
-      priority: 'medium',
-    },
-  ];
+  const { data: projects } = trpc.project.getAll.useQuery();
+  const { data: tickets } = trpc.ticket.getAll.useQuery();
+  const { data: ticketPriorities } = trpc.ticket.priority.getAll.useQuery();
+  const { data: ticketStatuses } = trpc.ticket.status.getAll.useQuery();
+  const { data: ticketTypes } = trpc.ticket.type.getAll.useQuery();
 
   const data = (projects ?? []).map((project) => {
-    const project_tickets = tickets.filter(
-      (ticket) => ticket.project === project.name
+    const project_data: { [key: string]: Project | number } = { project };
+
+    if (!tickets || !ticketPriorities || !ticketStatuses || !ticketTypes)
+      return project_data;
+
+    const project_tickets = tickets?.filter(
+      (ticket) => ticket.projectId === project.id
     );
 
-    return {
-      project,
-      status_backlog: project_tickets.filter(
-        (ticket) => ticket.status === 'backlog'
-      ).length,
-      status_code: project_tickets.filter((ticket) => ticket.status === 'code')
-        .length,
-      status_test: project_tickets.filter((ticket) => ticket.status === 'test')
-        .length,
-      status_refactor: project_tickets.filter(
-        (ticket) => ticket.status === 'refactor'
-      ).length,
-      type_chore: project_tickets.filter((ticket) => ticket.type === 'chore')
-        .length,
-      type_feat: project_tickets.filter((ticket) => ticket.type === 'feat')
-        .length,
-      type_fix: project_tickets.filter((ticket) => ticket.type === 'fix')
-        .length,
-      priority_urgent: project_tickets.filter(
-        (ticket) => ticket.priority === 'urgent'
-      ).length,
-      priority_high: project_tickets.filter(
-        (ticket) => ticket.priority === 'high'
-      ).length,
-      priority_medium: project_tickets.filter(
-        (ticket) => ticket.priority === 'medium'
-      ).length,
-      priority_low: project_tickets.filter(
-        (ticket) => ticket.priority === 'low'
-      ).length,
-    };
+    ticketPriorities.forEach(
+      (priority) =>
+        (project_data[`priority_${priority.value}`] = project_tickets.filter(
+          (ticket) => ticket.ticketPriorityId === priority.id
+        ).length)
+    );
+    ticketStatuses.forEach(
+      (status) =>
+        (project_data[`status_${status.value}`] = project_tickets.filter(
+          (ticket) => ticket.ticketStatusId === status.id
+        ).length)
+    );
+    ticketTypes.forEach(
+      (type) =>
+        (project_data[`type_${type.value}`] = project_tickets.filter(
+          (ticket) => ticket.ticketTypeId === type.id
+        ).length)
+    );
+
+    return project_data;
   });
 
   return (
@@ -122,55 +74,72 @@ function Index() {
       <div className="items-center bg-slate-50 flex font-medium justify-between p-4 rounded-lg shadow-md">
         <span className="text-xl">Active Projects</span>
         <div className="bg-blue-200 border-2 border-blue-800 rounded-full px-3 py-1 text-blue-800">
-          {projects?.length ?? 0}
+          {projects?.filter((project) => !project.archived).length ?? 0}
         </div>
       </div>
       <div className="items-center bg-slate-50 flex font-medium justify-between p-4 rounded-lg shadow-md">
         <span className="text-xl">Urgent Tickets</span>
         <div className="bg-red-200 border-2 border-red-800 rounded-full px-3 py-1 text-red-800">
-          {tickets.filter((ticket) => ticket.priority === 'urgent').length}
+          0
         </div>
       </div>
       <div className="items-center bg-slate-50 flex font-medium justify-between p-4 rounded-lg shadow-md">
         <span className="text-xl">Unresolved Tickets</span>
         <div className="bg-orange-200 border-2 border-orange-800 rounded-full px-3 py-1 text-orange-800">
-          {tickets.filter((ticket) => ticket.status !== 'done').length}
+          {tickets ? tickets.filter((ticket) => !ticket.archived).length : 0}
         </div>
       </div>
       <div className="items-center bg-slate-50 flex font-medium justify-between p-4 rounded-lg shadow-md">
         <span className="text-xl">Unassigned Tickets</span>
         <div className="bg-purple-200 border-2 border-purple-800 rounded-full px-3 py-1 text-purple-800">
-          {tickets.filter((ticket) => ticket.assignee === undefined).length}
+          {tickets ? tickets.filter((ticket) => !ticket.assignedId).length : 0}
         </div>
       </div>
       <div className="bg-slate-50 col-span-4 flex flex-col p-4 rounded-lg shadow-md space-y-2">
         <span className="font-medium text-xl text-slate-900">Tickets</span>
-        <ResponsiveContainer height={500} width="100%">
-          <BarChart
-            data={data}
-            margin={{ bottom: 0, left: 0, right: 0, top: 20 }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="project" />
-            <YAxis />
-            <Tooltip />
-            <Bar stackId="status" dataKey="status_backlog" fill="red" />
-            <Bar stackId="status" dataKey="status_code" fill="orange" />
-            <Bar stackId="status" dataKey="status_test" fill="yellow" />
-            <Bar stackId="status" dataKey="status_refactor" fill="green" />
-            <Bar stackId="type" dataKey="type_chore" fill="red" />
-            <Bar stackId="type" dataKey="type_feat" fill="orange" />
-            <Bar stackId="type" dataKey="type_fix" fill="yellow" />
-            <Bar stackId="priority" dataKey="priority_urgent" fill="red" />
-            <Bar stackId="priority" dataKey="priority_high" fill="orange" />
-            <Bar stackId="priority" dataKey="priority_medium" fill="yellow" />
-            <Bar stackId="priority" dataKey="priority_low" fill="green" />
-          </BarChart>
-        </ResponsiveContainer>
+        {!tickets || !ticketPriorities || !ticketStatuses || !ticketTypes ? (
+          <div>Loading...</div>
+        ) : (
+          <ResponsiveContainer height={500} width="100%">
+            <BarChart
+              data={data}
+              margin={{ bottom: 0, left: 0, right: 0, top: 20 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="project" />
+              <YAxis />
+              <Tooltip />
+              {ticketStatuses.map((status) => (
+                <Bar
+                  key={status.id}
+                  stackId="status"
+                  dataKey={`status_${status.value}`}
+                  fill={status.colour}
+                />
+              ))}
+              {ticketTypes.map((type) => (
+                <Bar
+                  key={type.id}
+                  stackId="type"
+                  dataKey={`type_${type.value}`}
+                  fill={type.colour}
+                />
+              ))}
+              {ticketPriorities.map((priority) => (
+                <Bar
+                  key={priority.id}
+                  stackId="priority"
+                  dataKey={`priority_${priority.value}`}
+                  fill={priority.colour}
+                />
+              ))}
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </div>
       <div className="bg-slate-50 col-span-4 p-4 rounded-lg shadow-md">
         <span className="font-medium text-xl text-slate-900">Projects</span>
-        <ProjectList isLoading={isLoading} projects={projects} />
+        <ProjectList isLoading={!projects} projects={projects} />
       </div>
     </div>
   );
