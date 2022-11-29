ff64b5a5e1bf49c1c450cd086bd90f94f2845d6c
iamryanmacdonald <ram_idts@me.com>
2022-11-29 12:06:16 +1100

    wip: #60 added middleware to handle necessary redirects

--- apps/issues/lib/auth.ts
+++ apps/issues/lib/auth.ts
@@ -8,11 +8,25 @@ import { env } from '../server/env';
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    async session({ session, user }) {
+    async jwt({ token, user }) {
+      if (!token.email) return token;
+
+      const dbUser = await prisma.user.findUnique({
+        where: { email: token.email },
+      });
+
+      if (!dbUser) return { id: user?.id, ...token };
+
+      const { email, id, image, name, settings } = dbUser;
+
+      return { email, id, name, picture: image, settings };
+    },
+    async session({ session, token }) {
       if (session.user) {
-        session.user.id = user.id;
-        session.user.settings = user.settings;
+        session.user.id = (token.id as string) ?? '';
+        session.user.settings = token.settings ?? {};
       }
+
       return session;
     },
   },
@@ -22,4 +36,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
 };
