182b402e21cfb0b5922d813a1d2355f438f1d42b
iamryanmacdonald <ram_idts@me.com>
2022-12-07 15:02:23 +1100

    wip: #60 moved add organization page

---
+++ apps/issues/app/organizations/add/page.tsx
@@ -0,0 +1,9 @@
+import OrganizationForm from '../../../components/OrganizationForm';
+
+export default async function Page() {
+  return (
+    <div className="bg-slate-200 h-screen pt-4">
+      <OrganizationForm />
+    </div>
+  );
+}
