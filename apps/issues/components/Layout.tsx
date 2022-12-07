67a98ae00566a6942bcd22936040742c830a2e55
iamryanmacdonald <ram_idts@me.com>
2022-12-07 15:05:39 +1100

    wip: #60 removed old files

--- apps/issues/components/Layout.tsx
+++ apps/issues/components/Layout.tsx
@@ -1,7 +1,7 @@
 import { Organization } from '@prisma/client';
 import { Session } from 'next-auth';
 
-import { Sidebar } from './Sidebar_OLD';
+import { Sidebar } from './Sidebar';
 
 export default function Layout({
   children,
